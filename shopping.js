41cf9d5a46eb676cc6da47905266c68543d5f05a
jhokamp <jmhokamp@gmail.com>
2025-06-10 20:20:50 -0500

    Update

---
+++ shopping.js
@@ -0,0 +1,7 @@
+const addButton = document.getElementById(`addButton`);
+const itemButton = document.getElementById(`itemButton`);
+const shoppingList = document.getElementById(`shoppingList`);
+
+addButton.addEventListener("click", () => {
+    
+})
